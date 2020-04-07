fd92e5c114ebfbe81a7c0c33e43ed4ba3a68bc88
Alisha Patel <alishapatel@Alishas-MacBook.hub>
2020-04-07 11:20:47 +1000

    added ticker

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ticker from 'react-ticker';
 import "./Home.css";
 
 
@@ -7,7 +8,7 @@ function Home () {
     <div>
       <div className="section-container hero">
         <div className="content-container intro">
-          <h1 className="heading">
+          <h1 className="heading fade-in">
             Hi, I’m Alisha, a UX 
             designer based in 
             Sydney , Australia.
@@ -15,12 +16,18 @@ function Home () {
         </div>
       </div>
     
-      <p className="banner">
-        Designer. Developer. Creator :)
-      </p>
+      <div className="banner fade-in">
+        <Ticker>
+          {() => (
+              <>
+                  <p style={{paddingRight: '1em'}}>Designer. Developer. Creator :)</p>
+              </>
+          )}
+        </Ticker>
+      </div>
  
       <div className="section-container skills">
-        <div className="content-container skillset">
+        <div className="content-container skillset fade-in">
           <p className="skills-label">Skills</p>
           <ul className="skills-list">
             <li>> UX Research</li>
